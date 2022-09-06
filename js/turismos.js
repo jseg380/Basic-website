6eeb26ae723a0251c62ed67bf75263af83a4f383
Juan <juanmalan10@gmail.com>
2022-09-06 22:23:11 +0200

    Added grid layout to the image viewer

--- js/turismos.js
+++ js/turismos.js
@@ -14,24 +14,31 @@ window.addEventListener('load', function () {
 
   closeBtn.addEventListener('click', CloseImage);
   
+
+  // Functions
+
   function ShowImage(e) {
     e.preventDefault();
 
     const selectedImg = e.target;
     
     const imgSrc = selectedImg.getAttribute('src').replace('_small', '');
-    console.log(imgSrc);
+
     bigImg.setAttribute('src', imgSrc);
     bigImg.setAttribute('alt', selectedImg.getAttribute('alt'));
     bigImg.setAttribute('title', selectedImg.getAttribute('title'));
     desc.innerText = bigImg.getAttribute('title');
     
-    bigImgDiv.style.display = 'flex';
+    bigImgDiv.style.display = 'grid';
+    bigImg.scrollIntoView({behavior: 'smooth'});
   }
 
   function CloseImage(e) {
     e.preventDefault();
 
     bigImgDiv.style.display = 'none';
+    bigImg.setAttribute('src', '');
+    bigImg.setAttribute('alt', '');
+    bigImg.setAttribute('title', '');
   }
 });
